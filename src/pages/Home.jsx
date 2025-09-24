e00051075c7ad608513174d1c79b8fb44d5acf4b
Alberto <dany.canaviri2022@gmail.com>
2025-09-23 23:56:07 -0400

    Tailwind setup configuration, configuration App File, completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-blue-500">
+            {/* Theme Toggle */}
+            
+            {/* Background Effect */}
+
+            {/* Navbar */}
+
+            {/* Main Content */}
+
+            {/* Footer */}
+        </div>
+    ); 
+}
